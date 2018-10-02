dc8c8f3dfc907b33bc320a6cb578a807d334cf82
hanyonghee <hanyonghee9264@gmail.com>
2018-10-02 16:43:13 +0900

    try-except구문을 사용해서 objects.get()에서 발생하는 DoesNotExist예외처리

--- app/polls/views.py
+++ app/polls/views.py
@@ -16,7 +16,16 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+
+    # question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
