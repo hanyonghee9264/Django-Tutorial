4194dc7597c473d0e19a7ea157cde699eda7e9b6
hanyonghee <hanyonghee9264@gmail.com>
2018-10-04 12:37:28 +0900

    result.html에 투표다시하기와 목록으로 돌아가기

--- app/polls/views/__init__.py
+++ app/polls/views/__init__.py
@@ -28,8 +28,18 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # question_id에 해당하는 Question인스턴스를 render함수의 context로 전달
+    # template은 'polls/results.html'을 사용
+
+    # 1. Template에서는 전달받은 Question인스턴스에 속하는 Choice목록을 순회하며 보여줌
+    #  이 때, 각 Choice아이템들의 'choice_text' 및 'votes'속성 값도 같이 출력
+
+    # 2. 'Return to polls index'링크 만들기 (polls.views.index로 돌아가면 됨)
+    context = {
+        'question': get_object_or_404(Question, pk=question_id),
+
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
